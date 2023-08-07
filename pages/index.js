c63108763eb6d313c424c594ef6722f5fa3c59a0
Harshita Patni <137406431+harshita117113@users.noreply.github.com>
2023-08-07 11:57:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,12 @@ export default function Home() {
   return (
     <>
     <Head>
+      <title>Electronic Gadgets</title>
+      <meta charset="UTF-8"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="Discover a wide range of electronic gadgets at affordable prices, all in one place. Shop now and experience convenience like never before."></meta>
+      <meta name="keywords" content="Electronic, Gadgets, buy, lowest,best,laptop,earphones,earbuds,speaker,smart,watch,headphone"></meta>
+
     </Head>
     <Header/>
     <div className={styles.electronicsGadgets}>Electronics Gadgets</div>
